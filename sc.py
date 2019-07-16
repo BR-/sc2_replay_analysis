e2dada23a7362e09e2849181003fa0a9af77e790
Benjamin <wishingwellwellwishing@hotmail.com>
2019-07-15 17:16:34 -0700

    separate counts by race

--- sc.py
+++ sc.py
@@ -8,7 +8,8 @@ def dump(ev):
 			print(x,"=",repr(getattr(ev,x)))
 	print("\n\n")
 
-c = Counter()
+c = {"Terran": Counter(), "Zerg": Counter(), "Protoss": Counter()}
+races_games = Counter()
 total = len(list(glob(r"C:\Users\Benjamin\Documents\StarCraft II\Accounts\80393116\1-S2-1-3575269\Replays\Multiplayer\*")))
 print(f"parsing {total} replays...")
 for i,replayname in enumerate(glob(r"C:\Users\Benjamin\Documents\StarCraft II\Accounts\80393116\1-S2-1-3575269\Replays\Multiplayer\*")):
@@ -16,19 +17,23 @@ for i,replayname in enumerate(glob(r"C:\Users\Benjamin\Documents\StarCraft II\Ac
 		replay = sc2reader.load_replay(replayname)
 	except:
 		continue
-	#for x in replay.game_events:
-	#	if "ability_name" in dir(x):
-	#		c[x.ability_name] += 1
-	c.update(x.ability_name for x in replay.game_events if "ability_name" in dir(x))
+	if not replay.is_ladder:
+		continue
+	races_games.update(x.play_race for x in replay.players if x)
+	for x in replay.game_events:
+		if "ability_name" in dir(x):
+			c[x.player.play_race][x.ability_name] += 1
 	print(f"{int(i*100./total)}%")
 
-with open("output.json", "w") as fh:
-	fh.write("{\n")
-	f = True
-	for k,v in sorted(c.items(), key=lambda x:-x[1]):
-		if f:
-			f = False
-		else:
-			fh.write(",\n")
-		fh.write(f"'{k}': {v}")
-	fh.write("\n}\n")
+for race in ["Protoss", "Terran", "Zerg"]:
+	with open(f"{race}.json", "w") as fh:
+		fh.write("{\n")
+		fh.write(f"'total games': {races_games[race]},\n")
+		f = True
+		for k,v in sorted(c[race].items(), key=lambda x:-x[1]):
+			if f:
+				f = False
+			else:
+				fh.write(",\n")
+			fh.write(f"'{k}': {v}")
+		fh.write("\n}\n")
