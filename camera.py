840e4376fa2f8f5156fcce6c0492686817ae3772
B R <wishingwellwellwishing@hotmail.com>
2019-07-20 05:38:19 -0700

    add multiprocessing to significantly speed up replay parsing

--- camera.py
+++ camera.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import sc2reader
 from pathlib import Path
 from collections import Counter, defaultdict
@@ -7,10 +8,7 @@ def dump(ev):
                        print(x,"=",repr(getattr(ev,x)))
        print("\n\n")
 
-cam_total = defaultdict(int)
-total = len(list(Path("replays").glob("**/*.SC2Replay")))
-print(f"parsing {total} replays...")
-for donesofar,replayname in enumerate(Path("replays").glob("**/*.SC2Replay")):
+def mp_process(replayname):
 	replay = sc2reader.load_replay(replayname.open("rb"))
 	coords = defaultdict(list)
 	for ev in replay.game_events:
@@ -21,13 +19,22 @@ for donesofar,replayname in enumerate(Path("replays").glob("**/*.SC2Replay")):
 		for pos1, pos2 in zip(cams, cams[1:]):
 			if (pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2 > 9:
 				camera_locations[player].append(pos2)
+	cam_thisrun = Counter()
 	for player,cams in camera_locations.items():
 		for i,count in enumerate(x[1] for x in Counter(cams).most_common()):
 			if count <= 1:
 				break
-			cam_total[i] += count
+			cam_thisrun[i] += count
 			# this could be normalized by multiplying by:
 			#  60. / replay.game_length.seconds
-	print(f"{donesofar}/{total} = {100*donesofar/total:0.0f}%")
+	return cam_thisrun
 
-print(cam_total)
+if __name__ == "__main__":
+	cam_total = Counter()
+	total = len(list(Path("replays").glob("**/*.SC2Replay")))
+	print(f"parsing {total} replays...")
+	with multiprocessing.Pool() as pool:
+		for donesofar,cam_thisrun in enumerate(pool.imap_unordered(mp_process, Path("replays").glob("**/*.SC2Replay"))):
+			print(f"{donesofar}/{total} = {100*donesofar/total:0.0f}%")
+			cam_total.update(cam_thisrun)
+	print(cam_total)
