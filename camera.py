3762813b9935f382cee6e118d4447698074be536
B R <wishingwellwellwishing@hotmail.com>
2019-07-20 05:13:25 -0700

    filter observers and refs from camera data

--- camera.py
+++ camera.py
@@ -13,15 +13,15 @@ for i,replayname in enumerate(Path("replays").glob("**/*.SC2Replay")):
 	replay = sc2reader.load_replay(replayname.open("rb"))
 	coords = defaultdict(list)
 	for ev in replay.game_events:
-		if ev.name == "CameraEvent":
-			coords[ev.pid].append((ev.x, ev.y))
+		if ev.name == "CameraEvent" and not ev.player.is_observer and not ev.player.is_referee:
+			coords[ev.player].append((ev.x, ev.y))
 	camera_locations = defaultdict(list)
-	for pid,cams in coords.items():
+	for player,cams in coords.items():
 		for pos1, pos2 in zip(cams, cams[1:]):
 			if (pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2 > 9:
-				camera_locations[pid].append(pos2)
-	for pid,cams in camera_locations.items():
-		print(replay.players[pid].name, len(cams))
-		print(Counter(cams).most_common(10))
+				camera_locations[player].append(pos2)
+	for player,cams in camera_locations.items():
+		print(player.name, len(cams))
+		print([x[1] for x in Counter(cams).most_common(10)])
 		print()
 	print("==============================")
