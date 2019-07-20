3b65419ee83369efc057837449423c190734b02a
B R <wishingwellwellwishing@hotmail.com>
2019-07-20 05:32:46 -0700

    start outputting aggregate cam information

--- camera.py
+++ camera.py
@@ -7,9 +7,10 @@ def dump(ev):
                        print(x,"=",repr(getattr(ev,x)))
        print("\n\n")
 
+cam_total = defaultdict(int)
 total = len(list(Path("replays").glob("**/*.SC2Replay")))
 print(f"parsing {total} replays...")
-for i,replayname in enumerate(Path("replays").glob("**/*.SC2Replay")):
+for donesofar,replayname in enumerate(Path("replays").glob("**/*.SC2Replay")):
 	replay = sc2reader.load_replay(replayname.open("rb"))
 	coords = defaultdict(list)
 	for ev in replay.game_events:
@@ -21,7 +22,12 @@ for i,replayname in enumerate(Path("replays").glob("**/*.SC2Replay")):
 			if (pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2 > 9:
 				camera_locations[player].append(pos2)
 	for player,cams in camera_locations.items():
-		print(player.name, len(cams))
-		print([x[1] for x in Counter(cams).most_common(10)])
-		print()
-	print("==============================")
+		for i,count in enumerate(x[1] for x in Counter(cams).most_common()):
+			if count <= 1:
+				break
+			cam_total[i] += count
+			# this could be normalized by multiplying by:
+			#  60. / replay.game_length.seconds
+	print(f"{donesofar}/{total} = {100*donesofar/total:0.0f}%")
+
+print(cam_total)
